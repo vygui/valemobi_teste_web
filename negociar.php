79031ed99adaeab2815c33074201ae9c40298214
Davy Guilherme <vygui@yahoo.com.br>
2017-09-02 16:17:57 -0300

    Ambiente criado
    Configuração das bibliotecas

---
+++ negociar.php
@@ -0,0 +1,21 @@
+<html lang="pt-br">
+<head>
+	<title>Negociar</title>
+	<meta charset="utf-8" />
+	<!-- define a viewport -->
+	<meta name="viewport" content="width-device-width, initial-scale=1.0" />
+	<!-- adiciona CSS bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen" /> 
+	<!-- adiciona JQUERY -->
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<link rel="icon" href="img/short_icon.png" />
+	<link href="css/estilo_geral.css" rel="stylesheet" media="screen" />
+	<link href="css/estilo_home.css" rel="stylesheet" media="screen" />
+</head>
+<body>
+		
+	<!-- inclusão do JS bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
